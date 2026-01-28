6444d0417efda9df92b14ec39a67d0a14c80bd8a
gado7h <neomfilter.good@outlook.com>
2026-01-28 15:25:28 +0300

    chore: bump version to 1.1.1 and update defaults

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="roblox-test-runner",
-    version="1.1.0",
+    version="1.1.1",
     author="WildLink Team",
     description="Execute Luau tests on Roblox Cloud",
     long_description=long_description,
